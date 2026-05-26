b8667082535857f69fdea84ab543e0d6babff176
oryanend <ryanyamim@gmail.com>
2026-05-25 22:34:50 -0300

    feat: add new url api

--- frontend/frontend/src/environments/environment.ts
+++ frontend/frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8181/api/ai/ask',
+  apiUrl: 'https://checker-g8a2eybeddhwb2eb.brazilsouth-01.azurewebsites.net/api/ai/ask',
 };
